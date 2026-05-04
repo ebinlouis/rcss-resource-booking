d894b888b7a4877e0b281fd8690908fecc952e59
Sree <sreerajsr49@gmail.com>
2026-05-04 21:06:30 +0530

    feat: complete Phase 1 MVP with concurrency locks and Sentry

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,22 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView # <-- Added imports
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Swagger API Documentation
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), # The raw JSON map
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), # The beautiful UI
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), 
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), 
     
     # Core Infrastructure API
     path('api/auth/', include('apps.users.urls')),
-    path('api/approvals/', include('apps.approvals.urls')), # <-- The Master Queue!
+    path('api/approvals/', include('apps.approvals.urls')), 
     
     # Domain APIs
     path('api/spaces/', include('apps.spaces.urls')),
-    
-    # Your teammate's routes will go here once they finish!
     path('api/fleet/', include('apps.fleet.urls')),
     path('api/mess/', include('apps.mess.urls')),
     path('api/media/', include('apps.media.urls')),
